fc68f7063cadd86c4e941cd0bcbb8501228e739d
Ragnar Lonn <ragnar@lonn.org>
2012-11-16 17:22:29 +0100

    Working version

--- debian-installer/create-installer.sh
+++ debian-installer/create-installer.sh
@@ -9,10 +9,12 @@ die() {
 
 # Source files
 CONTROL="src/DEBIAN/control"
-AGENT_FILES="li_metrics_agent.py README LICENSE socket27.py httplib27.py"
-AGENT_CONF_SAMPLE="li_metrics_agent.conf.sample"
+AGENT_FILES="../li_metrics_agent.py ../README ../LICENSE ../socket27.py ../httplib27.py"
+AGENT_CONF_SAMPLE="../li_metrics_agent.conf.sample"
+INIT_D_FILE="src/etc/init.d/li_metrics_agent"
+INSTALLSCRIPTS="src/DEBIAN/preinst src/DEBIAN/postinst"
 
-VERSION=`grep ^Version: ${CONTROL} |awk {print $2}`
+VERSION=`grep ^Version: ${CONTROL} |awk '{print $2}'`
 
 TARGET="li_metrics_agent-${VERSION}_all"
 MD5SUMS="${TARGET}/DEBIAN/md5sums"
@@ -22,23 +24,35 @@ MD5SUMS="${TARGET}/DEBIAN/md5sums"
 # Make build directory
 mkdir ${TARGET} || die "Failed to create ${TARGET} directory. Do you have permissions?"
 
+# Make subdirs
+mkdir ${TARGET}/usr || die "Failed to create ${TARGET}/usr directory. Do you have permissions?"
+mkdir ${TARGET}/usr/lib || die "Failed to create ${TARGET}/usr/lib directory. Do you have permissions?"
+mkdir ${TARGET}/usr/lib/li_metrics_agent || die "Failed to create ${TARGET}/usr/lib/li_metrics_agent directory. Do you have permissions?"
+mkdir ${TARGET}/etc || die "Failed to create ${TARGET}/etc directory. Do you have permissions?"
+mkdir ${TARGET}/etc/init.d || die "Failed to create ${TARGET}/etc/init.d directory. Do you have permissions?"
+mkdir ${TARGET}/etc/li_metrics_agent || die "Failed to create ${TARGET}/etc/li_metrics_agent directory. Do you have permissions?"
+mkdir ${TARGET}/DEBIAN || die "Failed to create ${TARGET}/DEBIAN directory. Do you have permissions?"
+
 # Copy /usr/lib files
-mkdir -p ${TARGET}/usr/lib/li_metrics_agent || die "Failed to make directories under ${TARGET}/usr/lib"
-cp ${AGENT_FILES} ${TARGET}/usr/lib/li_metrics_agent || die "Failed to copy files to ${TARGET}/usr/lib"
+cp ${AGENT_FILES} ${TARGET}/usr/lib/li_metrics_agent || die "Failed to copy files to ${TARGET}/usr/lib/li_metrics_agent"
 
 # Copy /etc files
-cp -r src/etc ${TARGET}/etc || die "Failed to copy src/etc files to ${TARGET}/etc"
-cp -r ${AGENT_CONF_SAMPLE} ${TARGET}/etc/li_metrics_agent/${AGENT_CONF_SAMPLE} || die "Failed to copy ${AGENT_CONF_SAMPLE} to ${TARGET}/etc/li_metrics_agent"
+cp ${INIT_D_FILE} ${TARGET}/etc/init.d || die "Failed to copy ${INIT_D_FILE} file to ${TARGET}/etc/init.d"
+cp ${AGENT_CONF_SAMPLE} ${TARGET}/etc/li_metrics_agent || die "Failed to copy ${AGENT_CONF_SAMPLE} to ${TARGET}/etc/li_metrics_agent"
 
-# Copy /DEBIAN files
-cp -r src/DEBIAN ${TARGET}/DEBIAN || die "Failed to copy src/DEBIAN files to ${TARGET}/DEBIAN"
+# Copy /DEBIAN files (except control, generated below)
+cp ${INSTALLSCRIPTS} ${TARGET}/DEBIAN || die "Failed to copy ${INSTALLSCRIPTS} files to ${TARGET}/DEBIAN"
 
 # Generate checksum file
-md5sum `find ${TARGET} -type f | grep -v '^[.]/DEBIAN/'` |tee ${MD5SUMS}
+cd ${TARGET}
+md5sum `find . -type f | grep -v '^[.]/DEBIAN/'` |tee DEBIAN/md5sums
+cd ..
 
 # Generate Installed-Size value for control file
 SIZE=`du ${TARGET} |tail -1 |awk '{print $1}'`
-sed 's/^Installed-Size:.*/Installed-Size: '"${SIZE}/" src/DEBIAN/control >${CONTROL}
+
+# Generate control file
+sed 's/^Installed-Size:.*/Installed-Size: '"${SIZE}/" src/DEBIAN/control >${TARGET}/DEBIAN/control
 
 # Create package
 dpkg-deb -b ${TARGET}
